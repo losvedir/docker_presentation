72ade0fc7991d485a349be26d09935a031849a21
Gabe Durazo <gabe@mbtace.com>
2018-01-30 02:12:32 -0600

    Progress on slide 6

--- 6/demo/mix.exs
+++ 6/demo/mix.exs
@@ -22,6 +22,7 @@ defmodule Demo.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:distillery, "~> 1.5", runtime: false},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
